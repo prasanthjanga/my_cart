cc272d4876701c16a541feb9e5dc464df4344e7c
prasanthjanga <prasanth.janga@gmail.com>
2017-10-19 16:54:55 +0800

    Update routes.php

--- application/config/routes.php
+++ application/config/routes.php
@@ -50,7 +50,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |		my-controller/my-method	-> my_controller/my_method
 */
 // $route['login/index'] = 'login';
-$route['default_controller'] = 'my_cart/index';
+$route['default_controller'] = 'cart/index';
 
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
